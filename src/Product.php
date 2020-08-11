31d363d66ed40ecb2c982e956f49323c36add5a3
Edgariukstis <edgaras.banaitis@gmail.com>
2020-08-11 22:45:44 +0300

    first steps

---
+++ src/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /** 
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    /** 
+     * @ORM\Column(type="string") 
+     */
+    protected $name;
+
+
+
+    public function __construct()
+    {
+        $this->features = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
